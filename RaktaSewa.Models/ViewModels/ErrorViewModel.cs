7ce8ecc0a7a1b5f30519ba20a3a89693600069bf
SaRu-N <saruniroula73@gmail.com>
2022-08-10 22:22:59 +0545

    Project Setup

---
+++ RaktaSewa.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RaktaSewa.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
